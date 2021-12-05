f64b185cb6c29349cf2d428951dc35dac72d720e
SasiBory <sabrinaborysiuviera@gmail.com>
2021-12-05 14:58:19 -0300

    arreglo de alerta y foco

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
-function alerta(){
-    p = document.getElementById("principal").value;
-    if (p <= 0) { alert( "Enter a positive number")}
-}
-
 function compute()
 {
-var principal = document.getElementById("principal").value;
-var rate =  document.getElementById("rate").value;
-var years = document.getElementById("years").value;
-var interest = principal * years * rate /100;
-var year = new Date().getFullYear()+parseInt(years);
-document.getElementById("result").innerHTML = "If you deposit " + principal + ",\<br\>at an interest rate of " + rate + "%.\<br\>You will receive an amount of " + interest + ",\<br\>in the year " + year + "\<br\>"
+    var principal = document.getElementById("principal").value;
+
+    if(principal == "" || principal <= 0)
+    {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return;
+    }
+
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100;
+
+    var dateNow = new Date();
+    var yearNow = parseInt(dateNow.getFullYear()) + parseInt(years);
+    
+    var resultDisplay = document.getElementById("result");
+    resultDisplay.innerHTML = "If you deposit " + "<span class='highlight'>" + principal + "</span>."  + ", <br> at an interest rate of "+ "<span class='highlight'>" + rate + "</span>%." + "<br> You will receive an amount of " + "<span class='highlight'>" + interest + "</span>" + ", <br> in the year " + "<span class='highlight'>" + yearNow + "</span>";
 }
 
 function updateRate()
@@ -18,5 +25,6 @@ function updateRate()
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval + "%";
 }
+
 
 
